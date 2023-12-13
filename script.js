bedf1e45366fcc5ba0af17df290af4700077b52e
BIOMECJacob <jacob.rodriguez@biomec.com.co>
2023-12-13 15:04:23 -0500

    back ground

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-function move() {
-    var elem = document.getElementById("myBar");
-    var width = 0;
-    var id = setInterval(frame, 10);
-  
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-      } else {
-        width++;
-        elem.style.width = width + '%';
+window.onload = function() {
+  // Carga los datos del inventario y precios en la página
+  loadInventoryData();
+  loadPricesData();
+}
+
+function loadInventoryData() {
+  var inventario = {
+      pesca: {
+          cantidad: 800,
+          unidad: 'gr',
+          precio: 300,
+          precioUnitario: 375
       }
-    }
+  };
+
+  var inventarioDiv = document.getElementById('inventario');
+
+  for (var tipo in inventario) {
+      var item = inventario[tipo];
+      var divItem = document.createElement('div');
+      divItem.innerHTML = 'Cantidad: ' + item.cantidad + ' ' + item.unidad +
+                          '<br>Precio: ' + item.precio + ' €' +
+                          '<br>Precio unitario: ' + item.precioUnitario + ' €/kg';
+      inventarioDiv.appendChild(divItem);
   }
-  document.addEventListener('DOMContentLoaded', function () {
-    move(); // Llama a la función al cargar la página
-  });
-  
+}
+
+function loadPricesData() {
+  var precios = [
+      {semana: 1, precio: 350},
+      {semana: 2, precio: 300},
+      {semana: 3, precio: 250},
+      {semana: 4, precio: 200},
+      {semana: 5, precio: 150}
+  ];
+
+  var preciosDiv = document.getElementById('precios');
+
+  for (var i = 0; i < precios.length; i++) {
+      var precio = precios[i];
+      var divPrecio = document.createElement('div');
+      divPrecio.innerHTML = 'Semana ' + precio.semana + ': ' + precio.precio + ' €';
+      preciosDiv.appendChild(divPrecio);
+  }
+}

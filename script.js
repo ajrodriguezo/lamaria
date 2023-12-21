137ffedec85332f839828d1c01eb21dcd7b4ed59
JacobRodriguez <andresjacobr2@gmail.com>
2023-12-20 22:10:57 -0500

    avance

--- script.js
+++ script.js
@@ -9,3 +9,56 @@ document.addEventListener("DOMContentLoaded", function() {
   // Aplica el clip-path con la coordenada y calculada
   image.style.clipPath = `polygon(0 ${yCoordinate}%, 100% ${yCoordinate}%, 100% 100%, 0 100%)`;
 });
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var myChart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+            label: 'Ciclo 1',
+            data: [
+                { x: 1, y: 70 },
+                { x: 2, y: 140 },
+                { x: 3, y: 400 },
+                { x: 4, y: 1400 },
+                { x: 5, y: 2500 }
+            ],
+            backgroundColor: 'rgba(75, 192, 192, 0.5)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            x: {
+                type: 'linear',
+                position: 'bottom',
+                title: {
+                    display: true,
+                    text: 'Semanas'
+                }
+            },
+            y: {
+                type: 'linear',
+                position: 'left',
+                title: {
+                    display: true,
+                    text: 'Peso en gr'
+                }
+            }
+        },
+        plugins: {
+            title: {
+                display: true,
+                text: 'Ciclo 1'
+            }
+        }
+    }
+});
+
+
+document.getElementById('gracumulados').innerText = 'Nuevo Contenido 1';
+document.getElementById('faltante').innerText = 'Nuevo Contenido 1';
+document.getElementById('vendidoahoy').innerText = 'Nuevo Contenido 1';
+document.getElementById('promedioprecio').innerText = 'Nuevo Contenido 1';

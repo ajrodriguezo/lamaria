224fb9706a92ef7c7c35ed59002607bde6a941fd
CamiloP121 <andresospino14@gmail.com>
2023-12-23 21:00:11 -0500

    Crear tabla y columnas

---
+++ modules/orm.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import relationship
+
+from config.database import Base
+
+class Database(Base):
+    __tablename__ = 'LaMariaCosecha'
+
+    ciclo_id = Column(String, primary_key = True)
+    for c in range(1,21):
+        globals()[f"ciclo_{c}"] = Column(Float)

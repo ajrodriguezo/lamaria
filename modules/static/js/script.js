4d59976adf81d1ccc79b8ad32055d6ac5e59df99
JacobRodriguez <andresjacobr2@gmail.com>
2023-12-25 21:04:15 -0500

    cambio nav y maquetado datos

--- modules/static/js/script.js
+++ modules/static/js/script.js
@@ -64,13 +64,3 @@ document.getElementById('faltante').innerText = 'Nuevo Contenido 1';
 document.getElementById('vendidoahoy').innerText = 'Nuevo Contenido 1';
 document.getElementById('promedioprecio').innerText = 'Nuevo Contenido 1';
 
-
-/* Set the width of the sidebar to 250px (show it) */
-function openNav() {
-    document.getElementById("mySidepanel").style.width = "250px";
-}
-
-  /* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-}

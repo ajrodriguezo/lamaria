9c76148f4e9c46722b554746a2227aea178651aa
CamiloP121 <andresospino14@gmail.com>
2024-01-14 19:25:43 -0500

    cambiar repo docker

--- docker/start.sh
+++ docker/start.sh
@@ -3,7 +3,7 @@ docker build -t camilop121/lamarina:latest .
 
 docker run -it \
     --rm \
-    --net="host"\
-    --name="lamaria"\
+    --net="host" \
+    --name="lamaria" \
     -v $(pwd)/..:/lamaria \
-    camilop121/lamarina
+    camilop121/lamarina:latest
